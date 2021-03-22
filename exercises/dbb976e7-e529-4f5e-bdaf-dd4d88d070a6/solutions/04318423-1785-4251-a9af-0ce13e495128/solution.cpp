1445942ace69df34bbf314df35cfd7bb140e07cc
Davor Ljubenkov <dljube19@student.aau.dk>
2021-03-22 04:24:25 +0000

    created file exercises/dbb976e7-e529-4f5e-bdaf-dd4d88d070a6/solutions/04318423-1785-4251-a9af-0ce13e495128/solution.cpp

---
+++ exercises/dbb976e7-e529-4f5e-bdaf-dd4d88d070a6/solutions/04318423-1785-4251-a9af-0ce13e495128/solution.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <functional>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstdlib>
+
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
